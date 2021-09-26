02974925e0fed5708100dc6042f70c3f8e9830cd
Alpha <alphatero@icloud.com>
2021-09-26 20:03:40 +0800

    add new

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,88 @@
-import React, { useState } from 'react'
-import logo from './logo.svg'
-import poweredBy from './powered-by-vitawind-dark.png'
+import React, { cloneElement, Children } from 'react';
+import { Icon } from './components';
+import clsx from 'clsx';
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="text-center">
-      <header className="bg-[#282c34] min-h-screen text-white flex flex-col justify-center items-center">
-        <img
-          src={logo}
-          className="h-60 motion-safe:animate-spin animate-speed"
-          alt="logo"
-        />
-        <style>
-          {"\
-            .animate-speed{\
-              animation-duration:20s;\
-            }\
-          "}
-        </style>
-        <p className="text-3xl font-bold">Vite + React + Tailwind jit</p>
-        <p className="mt-3">
-          <button
-            type="button"
-            className="rounded text-[#282C34] bg-gray-300 px-2 py-2 my-6 hover:bg-gray-200 transition-all"
-            onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code className="text-[#8d96a7]">App.jsx</code> and save to test HMR updates.
-        </p>
-        <p className="mt-3 flex gap-3 text-center text-[#8d96a7]">
-          <a
-            className="text-[#61dafb] hover:text-blue-400 transition-all"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="text-[#61dafb] hover:text-blue-400 transition-all"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-        <img src={poweredBy} className="mx-auto my-8" alt="powered-by" />
-      </header>
-    </div>
-  )
+function Card({ children, className, icon }) {
+  return <div className={clsx('flex rounded-3xl', className)}>{children}</div>;
 }
 
-export default App
+function TextField({ id, label, icon }) {
+  return (
+    <div className="flex flex-col gap-2">
+      <label htmlFor={id} className="text-cyan-normal text-base">
+        {label}
+      </label>
+      <div className="relative flex items-center">
+        <input
+          id={id}
+          name={id}
+          type="number"
+          className={clsx(
+            'w-full p-2 bg-cyan-lightest rounded-md',
+            'outline-none focus:ring focus:ring-cyan-focus',
+            'text-2xl text-right text-cyan'
+          )}
+        />
+        {cloneElement(icon, { className: 'absolute w-4 my-2 mx-4' })}
+      </div>
+    </div>
+  );
+}
+
+function RadioGroup({ id, label, children }) {
+  return (
+    <fieldset>
+      <legend>{label}</legend>
+
+      <div>
+        {Children.map(children, (child) => cloneElement(child, { name: id }))}
+      </div>
+    </fieldset>
+  );
+}
+
+function Radio({ label, value, name }) {
+  return (
+    <div>
+      <label htmlFor={value}>{label}</label>
+      <input type="radio" name={name} id={value} value={value} />
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div className="">
+      <header className="flex justify-center my-12">
+        <h1 className="w-24 text-cyan-logo">
+          <Icon.Logo />
+        </h1>
+      </header>
+      <main>
+        <Card className="bg-white p-8">
+          <form>
+            {/* Bill */}
+            <TextField id="bill" label="Bill" icon={<Icon.Dollar />} />
+
+            {/* Select Tips Button */}
+            <RadioGroup label="Bill">
+              <Radio label="5%" value="5" />
+              <Radio label="10%" value="10" />
+              <Radio label="15%" value="15" />
+              <Radio label="25%" value="25" />
+              <Radio label="50%" value="50" />
+              <Radio label="Custom" value="custom" />
+            </RadioGroup>
+
+            {/* Number of People */}
+            <TextField id="people" label="Number of People" icon={<Icon.Person />} />
+
+            {/* Present */}
+          </form>
+        </Card>
+      </main>
+    </div>
+  );
+}
+
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,19 @@ module.exports = {
   purge: ['index.html','./src/**/*.{js,jsx,ts,tsx,vue,html}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        cyan: {
+          logo: `#3D6666`,
+          dollar: `#9EBBBD`,
+          DEFAULT: `#00474B`,
+          darkest: `hsl(183, 100%, 15%)`,
+          normal: `#5E7A7D`,
+          light: `hsl(185, 41%, 84%)`,
+          lightest: `hsl(189, 41%, 97%)`,
+          focus: `#26C2AE`
+        }
+      }
+    },
   },
-  plugins: [],
 }
